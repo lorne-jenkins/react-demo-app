c0afb5c004781a1d430169d73e0d2c3ca8c612ff
Lorne J <biglornej@gmail.com>
2020-08-21 17:32:04 -0600

    Swing 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Welcome from "./components/welcome/welcome";
+import Clock from "./components/clock/clock";
+import Contact from "./components/contact/contact";
+import Navigation from "./components/navigation/navigation";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* render the Navigation component */}
+        <Navigation />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Lorne" />}
+        />
+
+        <Route
+          path="/welcome/:name"
+          render={(props) => <Welcome name={props.match.params.name} />}
+        />
+
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    );
+  }
 }
-
 export default App;
