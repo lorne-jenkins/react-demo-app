907b1d8ab73183a06fd0faa352b763c0d8ea4ac5
Lorne J <biglornej@gmail.com>
2020-08-27 08:12:14 -0600

    forgot the 404 page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import "./App.css";
 import Welcome from "./components/welcome/welcome";
 import Clock from "./components/clock/clock";
 import Contact from "./components/contact/contact";
 import Navigation from "./components/navigation/navigation";
+import Nomatch from "./components/nomatch/nomatch";
 
 class App extends Component {
   render() {
     return (
+      // div was here
       <div>
-        {/* render the Navigation component */}
-        <Navigation />
-        <Route
-          exact
-          path="/"
-          render={(props) => <Welcome {...props} name="Lorne" />}
-        />
+       
+       <Navigation />
+        <Switch>
+      
+          <Route
+            exact
+            path="/"
+            render={(props) => <Welcome {...props} name="Lorne" />}
+          >
+            <Welcome name="Lorne" />
+          </Route>
 
-        <Route
-          path="/welcome/:name"
-          render={(props) => <Welcome name={props.match.params.name} />}
-        />
+          <Route
+            path="/welcome/:name"
+            render={(props) => <Welcome name={props.match.params.name} />}
+          ></Route>
 
-        <Route path="/clock" component={Clock} />
-        <Route path="/contact" component={Contact} />
+          <Route path="/clock" component={Clock}>
+            <Clock />
+          </Route>
+          <Route path="/contact" component={Contact}>
+            <Contact />
+          </Route>
+
+          <Route path="/nomatch" component={Nomatch}>
+            <Nomatch />
+          </Route>
+          <Redirect to="/nomatch" />
+        </Switch>
       </div>
     );
   }
