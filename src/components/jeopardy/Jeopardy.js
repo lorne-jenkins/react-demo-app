c1bc56e84791e6c6b94b31f26c1979b92bf73150
Lorne J <biglornej@gmail.com>
2020-08-28 15:58:26 -0600

    Part 1 Jeopardy

--- src/components/jeopardy/Jeopardy.js
+++ src/components/jeopardy/Jeopardy.js
@@ -1,60 +1,123 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 //import our service
 import JeopardyService from "../../jeopardyService";
 class Jeopardy extends Component {
   //set our initial state and set up our service as this.client on this component
-  constructor(props){
+  constructor(props) {
     super(props);
     this.client = new JeopardyService();
     this.state = {
-      data:{
-        "id": 0,
-        "answer": "",
-        "question": "",
-        "value": 0,
-        "airdate": "",
-        "created_at": "",
-        "updated_at": "",
-        "category_id": 0,
-        "game_id": null,
-        "invalid_count": null,
-        "category": {
-          "id": 0,
-          "title": "",
-          "created_at": "",
-          "updated_at": "",
-          "clues_count": 0
-        }
-      } ,
-      score: 0
-    }
+      submitted: false,
+      formData: {
+        userAnswer: "",
+      },
+      data: {
+        id: 0,
+        answer: "",
+        question: "",
+        value: 0,
+        airdate: "",
+        created_at: "",
+        updated_at: "",
+        category_id: 0,
+        game_id: null,
+        invalid_count: null,
+        category: {
+          id: 0,
+          title: "",
+          created_at: "",
+          updated_at: "",
+          clues_count: 0,
+        },
+      },
+      score: 0,
+    };
   }
-   //get a new random question from the API and add it to the data object in state
-   getNewQuestion() {
-    return this.client.getQuestion().then(result => {
+  handleChange = (event) => {
+    let formData = this.state.formData;
+    formData[event.target.name] = event.target.value;
+    this.setState({ formData });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      submitted: true,
+    });
+  };
+  resetForm = (event) => {
+    this.getNewQuestion();
+    this.setState({
+      submitted: false,
+      formData: {
+        userAnswer: "",
+      },
+    });
+  };
+
+  //get a new random question from the API and add it to the data object in state
+  getNewQuestion() {
+    return this.client.getQuestion().then((result) => {
       this.setState({
-        data: result.data[0]
-      })
-    })
+        data: result.data[0],
+      });
+    });
   }
   //when the component mounts, get a the first question
   componentDidMount() {
     this.getNewQuestion();
   }
 
-
   //display the results on the screen
   render() {
+    if (this.state.submitted) {
+      let theTruth;
+      console.log(this.state.data.answer.toLowerCase())
+      if (
+        this.state.formData.userAnswer.toLowerCase() ===
+        this.state.data.answer.toLowerCase()
+      ) {
+        theTruth = "Correctamundo!";
+        this.state.score = this.state.score + this.state.data.value;
+      } else {
+        this.state.score = this.state.score - this.state.data.value;
+        theTruth = "Wrongaroni!";
+      }
+      return (
+        <div>
+          Your answer is: {this.state.formData.userAnswer}, it is: {theTruth}
+          <br />
+          <button onClick={this.resetForm}>Get the next question </button>
+        </div>
+      );
+    }
     return (
       <div>
-        {/* {JSON.stringify(this.state.data)} */}
-        <strong>Question: </strong>{this.state.data.question} <br/>
-        <strong>Value: </strong>{this.state.data.value} <br/>
-        <strong>Category: </strong>{this.state.data.category.title} <br/>
-        <strong>Answer: </strong>{this.state.data.answer} <br/>
-        <strong>Score: </strong>{this.state.data.score}
-        
+        <strong>Question: </strong>
+        {this.state.data.question} <br />
+        <strong>Value: </strong>
+        {this.state.data.value} <br />
+        <strong>Category: </strong>
+        {this.state.data.category.title} <br />
+        <strong>Answer: </strong>
+        {this.state.data.answer} <br />
+        <strong>Score: </strong>
+        {this.state.score}
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>Who/what is:</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="userAnswer"
+                value={this.state.formData.userAnswer}
+              />
+            </div>
+            <button>Submit Answer</button> <br />
+            {this.state.formData.userAnswer}
+          </form>
+        </div>
       </div>
     );
   }
